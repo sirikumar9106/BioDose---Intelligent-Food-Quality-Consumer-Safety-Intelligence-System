4477685f132b3d82bf5c4f87b2e0a09bc246bb3c
Siri Kumar C S <sirikumarcs.2006.123@gmail.com>
2026-06-25 00:54:03 +0530

    Add EMAIL_TIMEOUT to settings

--- backend/biodose/settings.py
+++ backend/biodose/settings.py
@@ -137,6 +137,7 @@ EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "True") == "True"
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
 DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER or "noreply@biodose.com")
+EMAIL_TIMEOUT = 10
 
 if EMAIL_HOST_USER:
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
